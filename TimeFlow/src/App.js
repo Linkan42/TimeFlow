34006f39a7bb11ad13059447265bb87f179d11f0
Oskar Sundberg <oskarsundberg@live.se>
2023-09-14 10:12:36 +0200

    Move around a bit :)

---
+++ TimeFlow/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Coworkers} from './View/Coworkers'
+import {BackGround} from './View/BackGround'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Coworkers/>
+      <BackGround/>
+    </div>
+  );
+}
+
+export default App;
