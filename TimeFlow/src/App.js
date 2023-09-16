3908d8c0c5f7173fe4e77a624b0bce44f2f902dc
Oskar Sundberg <oskarsundberg@live.se>
2023-09-16 16:38:04 +0200

    tyyed to positon items on home screen

--- TimeFlow/src/App.js
+++ TimeFlow/src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import {CoworkerList} from './View/CoworkerList';
 //import {BackGround} from './View/BackGround';
 import './App.css';
-import { Container, backgroundColor } from '@mui/material';
+import { Container, Grid, ListItem, Stack} from '@mui/material';
 
 function App() {
   return (
-      <Container>
-          <CoworkerList sx={{ backgroundColor: 'ligthgray'}}>
-        </CoworkerList>
-      </Container>
+        <Container>
+          <Grid> 
+            <span className='header'>Navigation bar</span>
+            <Grid>
+              <Stack direction="row" spacing={1}>
+                <CoworkerList className='sidebar'> </CoworkerList>
+                <ListItem className='schema' sx={{ backgroundColor: 'blue'}}>kalender </ListItem>
+                <ListItem className='sidebar' sx={{ backgroundColor: 'red'}}>Nästa möte här </ListItem>
+              </Stack>
+            </Grid>
+            <span className='header'>Navigation bar</span>
+          </Grid>
+        </Container>
   );
 }
 
