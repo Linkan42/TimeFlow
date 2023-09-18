dfb49ec11a8c7853f4467736018c37d87682804b
Linus Savinainen <sali20jt@student.ju.se>
2023-09-18 15:19:54 +0200

    some kind of message list up

--- TimeFlow/src/View/Messages.js
+++ TimeFlow/src/View/Messages.js
@@ -1,5 +1,12 @@
 import { Container, createTheme, ThemeProvider } from "@mui/material";
 import React,{Component} from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 const theme = createTheme({
     palette: {
@@ -21,10 +28,76 @@ export class Messages extends Component {
                     boxShadow: 1,
                     borderRadius: 2,
                     p: 2,
-                    width: '100vw',
-                    height: '20vh'
+                    minWidth: '100vw',
+                    maxWidth: '100vw'
                 }}>
-                    messages here
+                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar alt="Remy Sharp" src="furby.png"/>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Brunch this weekend?"
+                            secondary={
+                            <React.Fragment>
+                                <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                                >
+                                Ali Connors
+                                </Typography>
+                                {" — I'll be in your neighborhood doing errands this…"}
+                            </React.Fragment>
+                            }
+                        />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar alt="Travis Howard" src="legoman.png" />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Summer BBQ"
+                            secondary={
+                            <React.Fragment>
+                                <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                                >
+                                to Scott, Alex, Jennifer
+                                </Typography>
+                                {" — Wish I could come, but I'm out of town this…"}
+                            </React.Fragment>
+                            }
+                        />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar alt="Cindy Baker" src="snailcat.png" />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Oui Oui"
+                            secondary={
+                            <React.Fragment>
+                                <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                                >
+                                Sandra Adams
+                                </Typography>
+                                {' — Do you have Paris recommendations? Have you ever…'}
+                            </React.Fragment>
+                            }
+                        />
+                        </ListItem>
+                    </List>
                 </Container>
             </ThemeProvider>
         )
@@ -32,3 +105,88 @@ export class Messages extends Component {
 }
 
 export default Messages;
+
+/* big thanks https://mui.com/material-ui/react-list/
+
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+export default function AlignItemsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Sandra Adams
+              </Typography>
+              {' — Do you have Paris recommendations? Have you ever…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
+
+*/
