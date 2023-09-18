9991f1eea1c98385838e4861875386bdb6155803
Oskar Sundberg <oskarsundberg@live.se>
2023-09-18 13:56:24 +0200

    Basic Calander

--- TimeFlow/src/View/Calendar.js
+++ TimeFlow/src/View/Calendar.js
@@ -1,32 +1,15 @@
-import { Container, createTheme, ThemeProvider } from "@mui/material";
-import React,{Component} from "react";
 
-const theme = createTheme({
-    palette: {
-      background: {
-        paper: '#fff',
-      },
-      text: {
-        primary: '#173A5E',
-        secondary: '#46505A',
-    }
-}});
+import React,{Component} from "react";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
 export class Calandar extends Component {
     render(){
         return(
-            <ThemeProvider theme={theme}>
-            <Container sx={{
-            bgcolor: 'background.paper',
-            boxShadow: 1,
-            borderRadius: 2,
-            p: 2,
-            minWidth: 200,
-            width: '60%',
-            height: '88vh',
-            }}>
-                Calandar test här 
-            </Container>
-            </ThemeProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+          </LocalizationProvider>
         )
     }
 }
