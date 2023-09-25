05fc1d6ecb579ef280f04aeceb99ad109ce18fd4
Oskar Sundberg <oskarsundberg@live.se>
2023-09-25 10:07:00 +0200

    Some work on meetingScheduler

---
+++ src/View/TimeStack/MeetingStack/MeetingStack.js
@@ -0,0 +1,50 @@
+import { List, Grid, ListItemButton } from "@mui/material";
+import React,{Component} from "react";
+import './MeetingStack.css'; 
+
+export class MeetingStack extends Component {
+    render(){
+        return(
+            <List className="MeetingList">
+                {meetings.map( meeting => (
+                    <ListItemButton>
+                        <Grid background xs={12}>
+                            <Grid xs={3}> From: {meeting.from} to {meeting.to}</Grid>
+                            <Grid xs={6}>At {meeting.location}</Grid>
+                            <Grid xs={3}>{meeting.msg}</Grid>
+                        </Grid>
+                    </ListItemButton>
+                ))} 
+            </List>
+        )
+    }
+}
+
+const meetings = [
+    {
+        from: '11:00',
+        to: '12:00',
+        location: 'Hälso',
+        msg : 'Math?'
+    },
+    {
+      from: '11:00',
+      to: '12:00',
+      location: 'JTH',
+      msg : 'Scrum what to hate?'
+    },
+    {
+        from: '11:00',
+        to: '12:00',
+        location: 'JIBS',
+        msg : 'Scrum what to love?'
+      },
+      {
+        from: '11:00',
+        to: '12:00',
+        location: 'JTH',
+        msg : 'online'
+      },
+]
+
+export default MeetingStack;
--- src/View/TimeStack/TimeStack.js
+++ src/View/TimeStack/TimeStack.js
@@ -1,7 +1,9 @@
 
-import { Grid, Stack } from "@mui/material";
+import { Grid, Container, Stack} from "@mui/material";
 import React,{Component} from "react";
+import {CoworkerList} from '../CoworkerList/CoworkerList';
 import {TimeSelect} from './TimeSelect/TimeSelect';
+import {MeetingStack} from './MeetingStack/MeetingStack'
 import './TimeStack.css'
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -11,18 +13,24 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 export class TimeStack extends Component {
     render(){
         return(
-            <Grid>
-                <Stack direction="row">
-                <Grid>
-                    <Stack>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker/>
-                        </LocalizationProvider>
-                        <TimeSelect></TimeSelect>
-                    </Stack>
-                </Grid>
-                </Stack>
-            </Grid>
+            <Container className="BackGround">
+                <img src={require('./00035-1031831487.png')} alt="logo" class="centered-image"/>
+                <Grid className="block" container item xs={8}>
+                            <Grid item xs={4}>
+                                <CoworkerList className="CoworkerList"/>
+                            </Grid>
+                            <Grid item xs={4}>
+                          
+                                <Grid>
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DatePicker className="date"/>
+                                    </LocalizationProvider>
+                                    <TimeSelect className="std"/>  
+                                    <MeetingStack className="std"/>
+                                </Grid>                     
+                            </Grid>      
+                    </Grid>
+                </Container>
         )
     }
 }
