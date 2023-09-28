f84ec7e031db023d0c77e5682f79cd913e3468ba
Linus Savinainen <sali20jt@student.ju.se>
2023-09-28 14:37:52 +0200

    fix bugs

--- src/View/HeadPanel/HeadPanel.js
+++ src/View/HeadPanel/HeadPanel.js
@@ -1,6 +1,5 @@
 import { Button, Box} from "@mui/material";
 import React, { Component, useState } from "react";
-import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import dayjs from 'dayjs';
 
@@ -38,7 +37,7 @@ function DateButton() {
         if (buttonText === dayjs().format("dddd, DD MMMM YYYY"))
             setButtonText(currentDate.format("dddd, DD MMMM YYYY"));
         else
-            setButtonText(currentDate.format("YYYY-MM-D D"));
+            setButtonText(currentDate.format("YYYY-MM-DD"));
         weekDay = currentDate.format('dddd');
     }
 
@@ -47,11 +46,15 @@ function DateButton() {
         navigate("/meetingScheduler");
     }
 
+    const handleButtonLogout = () => {
+        navigate("/login");
+    }
+
   return (
          <>
 
             <Button sx={{width: '125px', boxShadow: 5, color: 'white', background: 'darkorange', top: 5}} variant="filledTonal" >
-            {weekDay}
+                {weekDay}
             </Button>
             <Button  className="ButtonL" variant="filledTonal" onClick={handleButtonClickLeft} sx={{top: 5, color: 'white'}}>
                 ←
@@ -60,13 +63,14 @@ function DateButton() {
                 →
             </Button>
             <Button className="dateButton" variant="primary" onClick={handleButtonClick} sx={{top: 5, color: 'white'}}> 
-            {buttonText}
+                {buttonText}
             </Button>
-            <Button sx={{position: 'absolute', right: 30, top: 5, color: 'white'}}>
+            <Button onClick={handleButtonLogout}
+                    sx={{position: 'absolute', right: 30, top: 5, color: 'white'}}>
                 LOGOUT
             </Button>
             <Button variant='contained' onClick={handleButtonNewMeeting}
-                    sx={{position: 'absolute', right: 115, top: 10, background: 'darkorange'}}>
+                    sx={{position: 'absolute', right: 115, top: 5, background: 'darkorange'}}>
                 + book meeting
             </Button>
         </>
@@ -77,16 +81,14 @@ export class HeadPanel extends Component {
     render () {
         return (
         <>
-        <Box sx={{
-            display: 'flow',
-            width: 'fill',
-            height: 50,
-            background: 'steelblue'
-                }}>
-                    <Link to="/login">
-                        <DateButton></DateButton>
-                    </Link>
-        </Box>
+            <Box sx={{
+                display: 'flow',
+                width: 'fill',
+                height: 50,
+                background: 'steelblue'
+                    }}>
+                <DateButton/>
+            </Box>
         </>
         )
 
