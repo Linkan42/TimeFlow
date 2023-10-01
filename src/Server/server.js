4b3ff77ef9a468f291c9e025fc6754fd762a9506
Oskar Sundberg <oskarsundberg@live.se>
2023-10-01 11:29:29 +0200

    Adding data to databas working

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const User = require('./user.js');
 
 const app = express();
 
-const uri = "mongodb+srv://Filmdados:TimeFlow@timeflow.bba95oe.mongodb.net/?retryWrites=true&w=majority";
 const my_path = '../../build/';
 
 // Serve static files from the build folder
@@ -16,9 +16,12 @@ app.get(['/', '/home', '/login', '/meetingScheduler', '/*'], (req, res) => {
     res.sendFile(path.join(__dirname, my_path, 'index.html'));
 });
 
-
 // Start the server
 app.listen(3001, () => console.log('Example app is listening on port 3001.'));
+
+//database stuff 
+
+const uri = "mongodb+srv://Filmdados:TimeFlow@timeflow.bba95oe.mongodb.net/?retryWrites=true&w=majority";
 
 async function connect(){
     try{
@@ -30,3 +33,22 @@ async function connect(){
     }
 }
 connect();
+
+const user = new User({
+    name: 'oskar',
+    password: '12345',
+    userId: 1
+});
+
+user.save()
+.then(() => {
+    console.log('User saved successfully');
+})
+.catch((err) => {
+    console.error('Error saving user:', err);
+});
+
+app.get('/', async (req, res) => {
+    const result = await user.find();
+    res.send({"users": result});
+});
