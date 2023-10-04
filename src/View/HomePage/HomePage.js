2975380557663f90789fa6a8d2a99a6090866a55
Linus Savinainen <sali20jt@student.ju.se>
2023-10-04 16:07:51 +0200

    dashboard rework up

--- src/View/HomePage/HomePage.js
+++ src/View/HomePage/HomePage.js
@@ -1,12 +1,10 @@
 import React,{Component} from "react";
 import {Grid} from "@mui/material";
 
-import {CoworkerList} from '../CoworkerList/CoworkerList';
 import {WeekDisplay} from '../WeekDisplay/WeekDisplay';
 import {NextMeeting} from '../NextMeeting/NextMeeting';
 import HeadPanel from "../HeadPanel/HeadPanel"
-
-import '../../App.css';
+import Profile from "../Profile/profile";
 
 export class HomePage extends Component {
     render(){
@@ -14,6 +12,17 @@ export class HomePage extends Component {
             <Grid container className="container">
                 <Grid item xs={12}>
                     <HeadPanel/>
+                </Grid>
+                <Grid item xs={7}>
+                    <WeekDisplay/>
+                </Grid>
+                <Grid item xs={5}>
+                    <Grid container item xs={12}>
+                        <NextMeeting/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Profile/>
+                    </Grid>
                 </Grid>
             </Grid>
             
