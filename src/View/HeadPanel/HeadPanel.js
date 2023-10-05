bf863355c1a1b1dee71dd3ea9963b37db11c4c61
Linus Savinainen <sali20jt@student.ju.se>
2023-10-05 14:09:59 +0200

    yeeted ivo's trash non-functioning functionality

--- src/View/HeadPanel/HeadPanel.js
+++ src/View/HeadPanel/HeadPanel.js
@@ -9,20 +9,7 @@ let currentDate = dayjs();
 let weekDay     = dayjs().format('dddd');
 
 function DateButton() {
-    const [buttonText, setButtonText] = useState(`${dayjs().format("dddd, DD MMMM YYYY")}`);
- 
-    if (weekDay === dayjs().format('dddd') && currentDate.format("D") === dayjs().format("D"))
-        weekDay = "Today";
-
-    const handleButtonClick = () => {
-        currentDate = dayjs();
-        if (buttonText === dayjs().format("dddd, DD MMMM YYYY"))
-            setButtonText(currentDate.format("YYYY-MM-DD"));
-        else
-            setButtonText(currentDate.format("dddd, DD MMMM YYYY"));
-        weekDay = currentDate.format('dddd');
-
-    };
+    const [buttonText, setButtonText] = useState(`${dayjs().format("YYYY-MM-DD")}`);
 
     const handleButtonClickLeft = () => { 
         currentDate = currentDate.add(-1, 'day');
@@ -65,7 +52,7 @@ function DateButton() {
                         <Button className="button" variant="filledTonal" onClick={handleButtonClickRight}>
                             →
                         </Button>
-                        <Button className="button" variant="filledTonal" onClick={handleButtonClick}> 
+                        <Button className="button" variant="filledTonal"> 
                             {buttonText}
                         </Button>
                     </Stack>
