a266746eed1b64a9b13a1cbff831948239712fdc
Linkan42 <sali20jt@student.ju.se>
2023-10-08 19:22:12 +0200

    minor changes

--- src/generate-secret-key.js
+++ src/generate-secret-key.js
@@ -1,4 +1,6 @@
 const crypto = require("crypto");
 
+// Generate 256 random bytes, which results in a string
+// of 512 random hexadecimal characters
 const newSecretKey = crypto.randomBytes(256).toString("hex");
 console.log(newSecretKey);
