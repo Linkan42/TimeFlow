b88211beaf18f2c13689c902437f084d46a7fcbc
Lukasydkvist <lukas.ydkvist@hotmail.com>
2023-10-09 13:36:22 +0200

    ran the linter

--- src/View/NextMeeting/NextMeeting.js
+++ src/View/NextMeeting/NextMeeting.js
@@ -12,38 +12,38 @@ const theme = createTheme({
 		}
 	}});
 export function NextMeeting(props) {
-    const [nextMeetingData, setNextMeetingData] = useState([])
+	const [nextMeetingData, setNextMeetingData] = useState([]);
 
-      useEffect(() => {
-        fetch('/api/meeting', { method: "GET" })
-          .then(response => response.json())
-          .then(data => {setNextMeetingData(data); console.log(data);})
-          .catch(error => {
-            console.error('Error fetching next meeting:', error);
-          });
-      }, []);
-    return (
-        <ThemeProvider theme={theme}>
-            <Container sx={{
-                    bgcolor: 'background.paper',
-                    boxShadow: 1,
-                    borderRadius: 2,
-                    p: 2,
-                    //minWidth: 200,
-                    //width: '20vw',
-                    maxHeight: '60vh',
-                    overflow: 'auto' 
-            }}>
-                <h1> Next Meeting </h1>
-                <h3>
-                Location: {nextMeetingData.length > 0 ? nextMeetingData[0].location : 'Loading...'}
-                </h3>
-                <h3>
-                Start Time: {nextMeetingData.length > 0 ? nextMeetingData[0].startTime : 'Loading...'}
-                </h3>
-            </Container>
-        </ThemeProvider>
-    );
+	useEffect(() => {
+		fetch("/api/meeting", { method: "GET" })
+			.then(response => response.json())
+			.then(data => {setNextMeetingData(data); console.log(data);})
+			.catch(error => {
+				console.error("Error fetching next meeting:", error);
+			});
+	}, []);
+	return (
+		<ThemeProvider theme={theme}>
+			<Container sx={{
+				bgcolor: "background.paper",
+				boxShadow: 1,
+				borderRadius: 2,
+				p: 2,
+				//minWidth: 200,
+				//width: '20vw',
+				maxHeight: "60vh",
+				overflow: "auto" 
+			}}>
+				<h1> Next Meeting </h1>
+				<h3>
+                Location: {nextMeetingData.length > 0 ? nextMeetingData[0].location : "Loading..."}
+				</h3>
+				<h3>
+                Start Time: {nextMeetingData.length > 0 ? nextMeetingData[0].startTime : "Loading..."}
+				</h3>
+			</Container>
+		</ThemeProvider>
+	);
 }
 
 export default NextMeeting;
