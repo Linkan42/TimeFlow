c41e6d306c548d1f69cecf8e00d688d44298f97b
Linkan42 <sali20jt@student.ju.se>
2023-10-10 18:23:21 +0200

    oops pushed to main xd

--- src/Views.jsx
+++ src/Views.jsx
@@ -1,15 +1,44 @@
-import {Routes, Route } from 'react-router-dom';
 import {HomePage} from './View/HomePage/HomePage.js';
 import SignIn from './View/LoginPage/Login.js'
 import {MeetingScheduler} from './View/MeetingScheduler/MeetingScheduler';
+
 const Views = () => {
+    const userToken = localStorage.getItem("token");
+    const tokenValid = userToken && userToken.length > 0;
+
+    /*
+    const ProtectedRoutes = (
+        <>
+            <Route path="/home" element={<HomePage/>} />
+            <Route path="/meetingScheduler"  element={<MeetingScheduler/>} />
+        </>
+    );
+    const UnProtectedRoutes = (
+        <>
+            <Route index element={<SignIn/>}/>
+            <Route path='*' element={<div> 404 LOL</div>} />
+        </>
+    );
+    */
+
+
     return (
     <Routes>
-        <Route path="/home" element={<HomePage />}/>
-        <Route path="/login" element={<SignIn />}/>
-        <Route path="/meetingScheduler"  element={<MeetingScheduler />}/>
-        <Route index element={<SignIn/>}/>
-        <Route path='*' element={<div> 404 LOL</div>} />
+        {tokenValid ? (
+            <>
+                <Route index element={<SignIn/>}/>
+                <Route path="/home" element={<HomePage/>}/>
+                <Route path="/meetingScheduler"  element={<MeetingScheduler/>}/>
+                <Route path='*' element={<div>404 LOL</div>}/>
+            </>
+        ) : (
+            <>
+                <Route index element={<SignIn/>}/>
+                <Route path="/home" element={<div> Please log in first!</div>}/>
+                <Route path="/meetingScheduler" element={<div> Please log in first!</div>}/>
+                <Route path='*' element={<div>404 LOL</div>}/>
+            </>
+        )}
     </Routes>
     );
 }
