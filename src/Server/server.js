bac44decf7e059d64fc6a1bdb04ae13299d5edf1
Oskar Sundberg <oskarsundberg@live.se>
2023-10-10 11:15:02 +0200

    fixed smal bug

--- src/Server/server.js
+++ src/Server/server.js
@@ -35,20 +35,25 @@ app.post("/api/meeting/save", async (req, res) => {
 		while(!check)
 		{	
 			meetingReturn = await MeetingProp.findOne({meetingId: meetingId});
-			console.log("while");
-			console.log(meetingReturn);
 			if(meetingReturn === null)
 			{
 				check = 1;
 			}
 		}
+		const token = req.header("Authorization").replace("Bearer ", "");
 
-
+		let decoded = null;
+		try {
+			decoded = jwt.verify(token, secretKey);
+		} catch (error) {
+			console.log("jwt.verify() failed: ", error);
+		}
+		const userId = decoded.userId;
 		const meetingProposal = new MeetingProp({meetingId: meetingId,
 			location:location, 
 			startTime:startTime, 
 			endTime:endTime,
-			createrUserId:1,
+			createrUserId:userId,
 			agenda:agenda,
 			date:date});
 		await meetingProposal.save();
@@ -158,15 +163,6 @@ app.post("/api/CreateUser", async (req, res) => {
 
 	try{
 		let id = await User.count() + 1;
-		/*
-        db.createUser(
-        {
-            user: "guest",
-            pwd: "passwordForGuest",
-            roles: [ { role: "read", db: "mydb" } ]
-        }
-        )
-        */
 		const user = new User({Email: Email,
 			Name: Name,
 			Password: Password,
